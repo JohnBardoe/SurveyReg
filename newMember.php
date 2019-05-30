982029a382ec5bb49d25198b3783e7bf2327c375
JohnBardoe <yta1@list.ru>
2019-05-30 22:29:49 +0300

    bs table change to cards

--- newMember.php
+++ newMember.php
@@ -1,20 +1,23 @@
 <?php
-function transliterate($string) {
-    $roman = array("Sch","sch",'Yo','Zh','Kh','Ts','Ch','Sh','Yu','ya','yo','zh','kh','ts','ch','sh','yu','ya','A','B','V','G','D','E','Z','I','Y','K','L','M','N','O','P','R','S','T','U','F','','Y','','E','a','b','v','g','d','e','z','i','y','k','l','m','n','o','p','r','s','t','u','f','','y','','e');
-    $cyrillic = array("Щ","щ",'Ё','Ж','Х','Ц','Ч','Ш','Ю','я','ё','ж','х','ц','ч','ш','ю','я','А','Б','В','Г','Д','Е','З','И','Й','К','Л','М','Н','О','П','Р','С','Т','У','Ф','Ь','Ы','Ъ','Э','а','б','в','г','д','е','з','и','й','к','л','м','н','о','п','р','с','т','у','ф','ь','ы','ъ','э');
-    return str_replace($cyrillic, $roman, $string);
-}
-
 error_reporting(-1);
 $db = mysqli_connect("127.0.0.1", "teemosha", "Letovo1", "survey");
 if($db->connect_error)
     die("Connection failed: " . $db->connect_error);
 if(isset($_POST)){
-    if(!mysqli_query($db, "INSERT IGNORE INTO participants (grade, name, surname, team) VALUES ('".$_POST['grade']."', '".transliterate($_POST['name'])."','".transliterate($_POST['surname'])."', '".$_POST['team']."' )")){
+    $name_ts = transliterator_transliterate('Russian-Latin/BGN', $_POST['name']);
+    $surname_ts = transliterator_transliterate('Russian-Latin/BGN', $_POST['surname']);
+    var_dump($name_ts, $surname_ts);
+    $grade = $_POST['grade'];
+    $team = $_POST['team'];
+    $search_res = $db->query("SELECT * FROM participants WHERE name='".$name_ts."'AND surname='".$surname_ts."'AND grade=".$grade);
+    if(mysqli_num_rows($search_res) == 0){
+    if(!mysqli_query($db, "INSERT IGNORE INTO participants (grade, name, surname, team) VALUES ('".$grade."', '".$name_ts."','".$surname_ts."', '".$team."' )")){
         echo "<img src='images/failure.gif'><br>".mysqli_error($db);
     }
     else{
         echo "<img src='images/success.gif'>";
     }
+    }
+    else echo "<img src='images/failure.gif'><br><br><h2>THE USER IS ALREADY REGISTERED</h2>";
 }
 ?>
